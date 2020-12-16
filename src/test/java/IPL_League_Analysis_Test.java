b83027ec993368717dccd3e3c059187c2d7ba80a
MURA199918 <muralisaranr18@gmail.com>
2020-12-16 18:45:56 +0530

    [Murali]Refactored to show cricketers with best Strike rates

--- src/test/java/IPL_League_Analysis_Test.java
+++ src/test/java/IPL_League_Analysis_Test.java
@@ -49,7 +49,7 @@ public class IPL_League_Analysis_Test {
         try{
             IPL_League ipl_league = new IPL_League();
             ipl_league.loadIPLRunsData(IPL_RUNS_CSV_FILE_PATH);
-            String sortedIPLRunsData = ipl_league.getTopSixes();
+            String sortedIPLRunsData = ipl_league.getTopFours();
             RunsFactSheet[] runsCSV = new Gson().fromJson(sortedIPLRunsData, RunsFactSheet[].class);
             System.out.println("Answer found");
             Assert.assertEquals(64, runsCSV[0].fours);
