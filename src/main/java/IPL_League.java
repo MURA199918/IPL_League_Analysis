46f78ff4a607891afebfaa2e251a63759c10ee4c
MURA199918 <muralisaranr18@gmail.com>
2020-12-16 15:48:59 +0530

    [Murali]Created IPL League Analysis program

---
+++ src/main/java/IPL_League.java
@@ -0,0 +1,5 @@
+public class IPL_League {
+    public static void main(String[] args) {
+        System.out.println(".....................Welcome to IPL League Analysis...................");
+    }
+}
---
+++ src/test/java/IPL_League_Analysis_Test.java
@@ -0,0 +1,3 @@
+public class IPL_League_Analysis_Test {
+
+}
